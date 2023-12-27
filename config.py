f8242dee480de94aaf049ebf53bf31c93fb27ec7
Developerjaypal <154951671+Developerjaypal@users.noreply.github.com>
2023-12-27 10:34:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,14 +10,14 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "35709a710e33be401047f08d0199b82b")
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "-1001928530174") 
 
-DB_NAME = os.environ.get("DB_NAME","")     
+DB_NAME = os.environ.get("DB_NAME","jaypal_thakor_148")     
 
 DB_URL = os.environ.get("DB_URL","mongodb+srv://jaypal_thakor_148:jaypal.008@mk40@atlascluster.fmnvlon.mongodb.net/?retryWrites=true&w=majority")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
-START_PIC = os.environ.get("START_PIC", "")
+START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/b1f145b52a2341c4db7d3.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5494468015').split()]
 
 PORT = os.environ.get("PORT", "8080")
